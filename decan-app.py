7673c2ba715c7cd9e1cec8afc8070de99074bd83
sebastiannoenning <sebastiannoenning@gmail.com>
2024-07-05 22:44:41 +0100

    added extra files

---
+++ decan-app.py
@@ -0,0 +1,13 @@
+import tkinter as tk
+import customtkinter as ctk
+
+class MainApp():
+    def __init__(self, master=None, model=None):
+        tk.Frame.__init__(self, master)
+        self.__init__
+
+def main():
+    
+    
+if __name__ == '__main__':
+    main()
