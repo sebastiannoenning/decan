398f7d38473e61ff6b5d28fdb01d5a49f52428fb
sebastianji <ec20425@qmul.ac.uk>
2024-07-07 16:01:51 +0100

    added bodyparser code

--- server/app.js
+++ server/app.js
@@ -1,7 +1,11 @@
 const express = require('express');
-const app = express();
+const bodyParser = require('body-parser')
 
+
+const app = express();
 const port = 3000;
+
+app.use(bodyParser.json())
 
 app.get('/', (req, res) => {
   res.send('Hello, World!');
