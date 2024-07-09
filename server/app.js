09655412bd28039d5dbb50fe21c9713dfef26e5e
sebastianji <ec20425@qmul.ac.uk>
2024-07-09 20:31:05 +0100

    added cors

--- server/app.js
+++ server/app.js
@@ -1,5 +1,7 @@
 const express = require('express');
-const bodyParser = require('body-parser')
+const cors = require('cors');
+const mysql = require('mysql');
+const bodyParser = require('body-parser');
 
 const app = express();
 const port = 3000;
