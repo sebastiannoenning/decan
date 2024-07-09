951df71fd7e82f8f3ce0b6e69f7c21cfc72d4773
sebastianji <ec20425@qmul.ac.uk>
2024-07-09 20:37:50 +0100

    c

--- server/app.js
+++ server/app.js
@@ -1,12 +1,19 @@
-const express = require('express');
-const cors = require('cors');
-const mysql = require('mysql');
-const bodyParser = require('body-parser');
+const express = require('express')
+const cors = require('cors')
+const mysql = require('mysql')
+const bodyParser = require('body-parser')
 
-const app = express();
-const port = 3000;
+const app = express()
+const port = 3000
 
 app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({extended:false}))
+
+const corsOptions = {
+  origin:'*',
+  credentials: true,
+  optionSuccessStatus: 200
+}
 
 app.get('/', (req, res) => {
   res.send('Hello, World!');
