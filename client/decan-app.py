f5939906a17d706d2f83213f3a778d7e9899c02f
sebastianji <ec20425@qmul.ac.uk>
2024-07-09 16:44:30 +0100

    added login page

--- client/decan-app.py
+++ client/decan-app.py
@@ -3,13 +3,13 @@ from tkinter import font as tkfont
 import customtkinter
 
 customtkinter.set_appearance_mode("Dark")  # Modes: "System" (standard), "Dark", "Light"
-customtkinter.set_default_color_theme("blue")  # Themes: "blue" (standard), "green", "dark-blue"
+customtkinter.set_default_color_theme("green")  # Themes: "blue" (standard), "green", "dark-blue"
 
 class CustomApp(customtkinter.CTk):
     def __init__(self, size=[800,480]):
         customtkinter.CTk.__init__(self)
 
-        self.title_font = customtkinter.CTkFont(family='Helvetica', size=18, weight="bold", slant="italic")
+        self.title_font = customtkinter.CTkFont(family='Helvetica', size=18, weight="bold")
         self.body_font = customtkinter.CTkFont(family='Helvetica', size=14)
         self.title("splash")
         self.width, self.height = size[0],size[1]
@@ -20,7 +20,7 @@ class CustomApp(customtkinter.CTk):
         page_container.grid_columnconfigure(0, minsize=self.width, weight=1)
         
         self.frames = {}
-        for F in (LoginPg, LoginPg):
+        for F in (LoginPg, DecanApplication):
             page_name = F.__name__
             frame = F(parent=page_container, controller=self)
             self.frames[page_name] = frame
@@ -43,105 +43,50 @@ class LoginPg(customtkinter.CTkFrame):
         self.title = 'Login Page'
         #self.label_image = tksvg.SvgImage(file="assets/orb.svg")
 
-        label = customtkinter.CTkLabel(self, text="Login", font=controller.title_font)
-        label.pack(side="top", fill="x", pady=10)
+        label_title = customtkinter.CTkLabel(self, text="Login", font=controller.title_font).grid(row=0,column=0,columnspan=2)
 
-        usernameentry = customtkinter.CTkEntry(master=self,placeholder_text="Username")
-        self.passwordentry = customtkinter.CTkEntry(master=self,placeholder_text="Password",show='*')
-        print(self.passwordentry.cget('show'))
-        self.vh_button = customtkinter.CTkButton(master=self, text='Show Password', command=self.change_format)
-        usernameentry.pack()
-        self.passwordentry.pack()
-        self.vh_button.pack()
-    
+        label_username = customtkinter.CTkLabel(self, text="Username", font=controller.body_font).grid(row=1,column=0,columnspan=2,sticky='w',padx=6)
+        self.entry_username = customtkinter.CTkEntry(master=self, placeholder_text="Username", corner_radius=0,)
+        self.entry_username.grid(row=2,column=0,columnspan=2,sticky='nsew')
+
+        label_password = customtkinter.CTkLabel(self, text="Password", font=controller.body_font).grid(row=3,column=0,columnspan=2,sticky='w',padx=6, pady=(2,0))
+        self.entry_password = customtkinter.CTkEntry(master=self, placeholder_text="Password", corner_radius=0, show='*')
+        self.entry_password.grid(row=4,column=0,sticky='nsew')
+        self.button_password = customtkinter.CTkButton(master=self, text='Show', corner_radius=0, command=self.change_format)
+        self.button_password.grid(row=4,column=1)
+
+        button_confirm = customtkinter.CTkButton(master=self,text="Confirm",command=self.login).grid(row=5, column=0, columnspan=2, sticky='nsew',pady=(6,0))
+
+    def login(self):
+        user = 'daniel'
+        password = 'danielspassword'
+        if self.entry_username.get()=='daniel' and self.entry_password.get()=='danielspassword':
+            print('Correct Login')
+            self.controller.show_frame("DecanApplication")
+        else: 
+            print('Invalid Login')
+
+
     def change_format(self):
-        match self.passwordentry.cget("show"):
+        match self.entry_password.cget("show"):
             case '*':
-                self.passwordentry.configure(show='')
-                self.vh_button.configure(text='Hide Password')
+                self.entry_password.configure(show='')
+                self.button_password.configure(text='Hide')
             case '':
-                self.passwordentry.configure(show='*')
-                self.vh_button.configure(text='Show Password')
-    
-        
+                self.entry_password.configure(show='*')
+                self.button_password.configure(text='Show')
 
-        
-
-
-
-        
-
-
-class MainApp(tk.Tk):
-
-    def __init__(self, *args, **kwargs):
-        tk.Tk.__init__(self, *args, **kwargs)
-
-        self.title_font = tkfont.Font(family='Helvetica', size=18, weight="bold", slant="italic")
-
-        # the container is where we'll stack a bunch of frames
-        # on top of each other, then the one we want visible
-        # will be raised above the others
-        container = tk.Frame(self, width=800, height=480)
-        container.pack(side="top", fill="both", expand=True)
-        container.grid_rowconfigure(0, minsize=480 ,weight=1)
-        container.grid_columnconfigure(0, minsize=800,weight=1)
-
-        self.frames = {}
-        for F in (LoginPage,PageOne, PageTwo):
-            page_name = F.__name__
-            frame = F(parent=container, controller=self)
-            self.frames[page_name] = frame
-
-            # put all of the pages in the same location;
-            # the one on the top of the stacking order
-            # will be the one that is visible.
-            frame.grid(row=0, column=0, sticky="nsew")
-
-        self.show_frame("LoginPage")
-
-    def show_frame(self, page_name):
-        '''Show a frame for the given page name'''
-        frame = self.frames[page_name]
-        frame.tkraise()
-
-class LoginPage(tk.Frame):
-
+class DecanApplication(customtkinter.CTkFrame):
     def __init__(self, parent, controller):
-        tk.Frame.__init__(self, parent)
+        customtkinter.CTkFrame.__init__(self, parent)
+
+        #Attributes
         self.controller = controller
-        label = tk.Label(self, text="This is the start page", font=controller.title_font)
-        label.pack(side="top", fill="x", pady=10)
-
-        usernameentry = tk.Entry(master=self)
-        passwordentry = tk.Entry(master=self)
-        usernameentry.pack()
-        passwordentry.pack()
-
-class PageOne(tk.Frame):
-
-    def __init__(self, parent, controller):
-        tk.Frame.__init__(self, parent)
-        self.controller = controller
-        label = tk.Label(self, text="This is page 1", font=controller.title_font)
-        label.pack(side="top", fill="x", pady=10)
-        button = tk.Button(self, text="Go to the start page",
-                           command=lambda: controller.show_frame("LoginPage"))
-        button.pack()
-
-class PageTwo(tk.Frame):
-
-    def __init__(self, parent, controller):
-        tk.Frame.__init__(self, parent)
-        self.controller = controller
-        label = tk.Label(self, text="This is page 2", font=controller.title_font)
-        label.pack(side="top", fill="x", pady=10)
-        button = tk.Button(self, text="Go to the start page",
-                           command=lambda: controller.show_frame("LoginPage"))
-        button.pack()
+        self.title = 'Login Page'
 
 
 
 if __name__ == "__main__":
+    print(tk.TkVersion)
     app = CustomApp()
     app.mainloop()
