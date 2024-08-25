4da836da79f34fbf342c17e5259baa7bef05ef1e
sebastianji <ec20425@qmul.ac.uk>
2024-08-25 17:04:33 +0100

    same

--- client/decan-pyqt/views/resources/custom_classes/eventlistview.py
+++ client/decan-pyqt/views/resources/custom_classes/eventlistview.py
@@ -9,13 +9,14 @@ from views.resources.custom_classes.eventitem import EventItem
 class EventListView(QFrame):
     def __init__(self, parent: QWidget) -> None:
         super().__init__(parent)
-        self.setup_ui
+        self.setup_ui()
 
     def setup_ui(self):
         self._list_container = QVBoxLayout()
         self._list_container.setSpacing(0)
         self._list_container.setContentsMargins(2)
         self.setLayout(self._list_container)
+        print('complete')
 
     def add_item(self, item: EventItem):
         _list = self._list_container
