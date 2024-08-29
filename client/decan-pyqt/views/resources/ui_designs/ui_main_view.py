f3c0f409c5c310381cb00d649868a87ffa559174
sebastianji <ec20425@qmul.ac.uk>
2024-08-29 21:30:21 +0100

    check location of object

--- client/decan-pyqt/views/resources/ui_designs/ui_main_view.py
+++ client/decan-pyqt/views/resources/ui_designs/ui_main_view.py
@@ -15,10 +15,11 @@ from PySide6.QtGui import (QBrush, QColor, QConicalGradient, QCursor,
     QFont, QFontDatabase, QGradient, QIcon,
     QImage, QKeySequence, QLinearGradient, QPainter,
     QPalette, QPixmap, QRadialGradient, QTransform)
-from PySide6.QtWidgets import (QApplication, QFrame, QHeaderView, QLabel,
-    QLineEdit, QListWidget, QListWidgetItem, QMainWindow,
+from PySide6.QtWidgets import (QApplication, QCheckBox, QComboBox, QDateTimeEdit,
+    QFormLayout, QGridLayout, QHBoxLayout, QHeaderView,
+    QLabel, QLineEdit, QListView, QMainWindow,
     QPushButton, QScrollArea, QSizePolicy, QStackedWidget,
-    QTableView, QWidget)
+    QTableView, QVBoxLayout, QWidget)
 
 from views.resources.custom_classes.eventlistview import EventListView
 
@@ -59,6 +60,7 @@ class Ui_MainWindow(object):
         self.userinfo = QStackedWidget(self.page_usr)
         self.userinfo.setObjectName(u"userinfo")
         self.userinfo.setGeometry(QRect(10, 30, 315, 420))
+        self.userinfo.setAutoFillBackground(False)
         self.p_user_login = QWidget()
         self.p_user_login.setObjectName(u"p_user_login")
         self.tb_Username = QLineEdit(self.p_user_login)
@@ -81,6 +83,7 @@ class Ui_MainWindow(object):
         self.tb_Password.setObjectName(u"tb_Password")
         self.tb_Password.setGeometry(QRect(10, 200, 274, 24))
         self.tb_Password.setFont(font)
+        self.tb_Password.setInputMethodHints(Qt.InputMethodHint.ImhHiddenText)
         self.tb_Password.setFrame(False)
         self.b_showhidebutton = QPushButton(self.p_user_login)
         self.b_showhidebutton.setObjectName(u"b_showhidebutton")
@@ -91,6 +94,7 @@ class Ui_MainWindow(object):
         sizePolicy.setHeightForWidth(self.b_showhidebutton.sizePolicy().hasHeightForWidth())
         self.b_showhidebutton.setSizePolicy(sizePolicy)
         self.b_showhidebutton.setMinimumSize(QSize(24, 24))
+        self.b_showhidebutton.setAutoFillBackground(False)
         self.l_Username = QLabel(self.p_user_login)
         self.l_Username.setObjectName(u"l_Username")
         self.l_Username.setGeometry(QRect(10, 110, 300, 20))
@@ -135,30 +139,164 @@ class Ui_MainWindow(object):
         self.Select.setGeometry(QRect(330, 0, 330, 20))
         self.Select.setFont(font2)
         self.Select.setAlignment(Qt.AlignmentFlag.AlignCenter)
-        self.list_users = QListWidget(self.page_usr)
-        self.list_users.setObjectName(u"list_users")
-        self.list_users.setGeometry(QRect(335, 30, 315, 420))
+        self.gridLayoutWidget = QWidget(self.page_usr)
+        self.gridLayoutWidget.setObjectName(u"gridLayoutWidget")
+        self.gridLayoutWidget.setGeometry(QRect(340, 50, 160, 80))
+        self.gridLayout = QGridLayout(self.gridLayoutWidget)
+        self.gridLayout.setObjectName(u"gridLayout")
+        self.gridLayout.setContentsMargins(0, 0, 0, 0)
         self.pages.addWidget(self.page_usr)
-        self.p_cal = QWidget()
-        self.p_cal.setObjectName(u"p_cal")
-        self.upcomingevents = QScrollArea(self.p_cal)
+        self.page_cal = QWidget()
+        self.page_cal.setObjectName(u"page_cal")
+        self.upcomingevents = QScrollArea(self.page_cal)
         self.upcomingevents.setObjectName(u"upcomingevents")
-        self.upcomingevents.setGeometry(QRect(10, 10, 341, 440))
+        self.upcomingevents.setGeometry(QRect(10, 10, 340, 440))
         self.upcomingevents.setVerticalScrollBarPolicy(Qt.ScrollBarPolicy.ScrollBarAlwaysOn)
         self.upcomingevents.setWidgetResizable(True)
-        self.scrollAreaWidgetContents = QWidget()
+        self.scrollAreaWidgetContents = EventListView()
         self.scrollAreaWidgetContents.setObjectName(u"scrollAreaWidgetContents")
-        self.scrollAreaWidgetContents.setGeometry(QRect(0, 0, 323, 438))
-        self.frame = EventListView(self.scrollAreaWidgetContents)
-        self.frame.setObjectName(u"frame")
-        self.frame.setGeometry(QRect(0, 0, 331, 441))
-        self.frame.setFrameShape(QFrame.Shape.StyledPanel)
-        self.frame.setFrameShadow(QFrame.Shadow.Raised)
+        self.scrollAreaWidgetContents.setGeometry(QRect(0, 0, 322, 438))
         self.upcomingevents.setWidget(self.scrollAreaWidgetContents)
-        self.table = QTableView(self.p_cal)
-        self.table.setObjectName(u"table")
-        self.table.setGeometry(QRect(360, 10, 291, 441))
-        self.pages.addWidget(self.p_cal)
+        self.pages.addWidget(self.page_cal)
+        self.page_addevent = QWidget()
+        self.page_addevent.setObjectName(u"page_addevent")
+        self.eventform = QScrollArea(self.page_addevent)
+        self.eventform.setObjectName(u"eventform")
+        self.eventform.setGeometry(QRect(310, 0, 341, 461))
+        self.eventform.setWidgetResizable(True)
+        self.scrollAreaWidgetContents_2 = QWidget()
+        self.scrollAreaWidgetContents_2.setObjectName(u"scrollAreaWidgetContents_2")
+        self.scrollAreaWidgetContents_2.setGeometry(QRect(0, 0, 339, 459))
+        self.verticalLayoutWidget = QWidget(self.scrollAreaWidgetContents_2)
+        self.verticalLayoutWidget.setObjectName(u"verticalLayoutWidget")
+        self.verticalLayoutWidget.setGeometry(QRect(0, 0, 331, 385))
+        self.generalcontainer = QVBoxLayout(self.verticalLayoutWidget)
+        self.generalcontainer.setObjectName(u"generalcontainer")
+        self.generalcontainer.setContentsMargins(0, 0, 0, 0)
+        self.GenericSettings = QFormLayout()
+        self.GenericSettings.setObjectName(u"GenericSettings")
+        self.label_user = QLabel(self.verticalLayoutWidget)
+        self.label_user.setObjectName(u"label_user")
+
+        self.GenericSettings.setWidget(0, QFormLayout.LabelRole, self.label_user)
+
+        self.selectuser = QComboBox(self.verticalLayoutWidget)
+        self.selectuser.setObjectName(u"selectuser")
+
+        self.GenericSettings.setWidget(0, QFormLayout.FieldRole, self.selectuser)
+
+        self.label_title = QLabel(self.verticalLayoutWidget)
+        self.label_title.setObjectName(u"label_title")
+
+        self.GenericSettings.setWidget(1, QFormLayout.LabelRole, self.label_title)
+
+        self.lineEdit = QLineEdit(self.verticalLayoutWidget)
+        self.lineEdit.setObjectName(u"lineEdit")
+
+        self.GenericSettings.setWidget(1, QFormLayout.FieldRole, self.lineEdit)
+
+        self.label_st = QLabel(self.verticalLayoutWidget)
+        self.label_st.setObjectName(u"label_st")
+
+        self.GenericSettings.setWidget(2, QFormLayout.LabelRole, self.label_st)
+
+        self.startedit = QDateTimeEdit(self.verticalLayoutWidget)
+        self.startedit.setObjectName(u"startedit")
+
+        self.GenericSettings.setWidget(2, QFormLayout.FieldRole, self.startedit)
+
+        self.label_et = QLabel(self.verticalLayoutWidget)
+        self.label_et.setObjectName(u"label_et")
+
+        self.GenericSettings.setWidget(3, QFormLayout.LabelRole, self.label_et)
+
+        self.endedit = QDateTimeEdit(self.verticalLayoutWidget)
+        self.endedit.setObjectName(u"endedit")
+
+        self.GenericSettings.setWidget(3, QFormLayout.FieldRole, self.endedit)
+
+
+        self.generalcontainer.addLayout(self.GenericSettings)
+
+        self.expandusers = QPushButton(self.verticalLayoutWidget)
+        self.expandusers.setObjectName(u"expandusers")
+
+        self.generalcontainer.addWidget(self.expandusers)
+
+        self.userview = QListView(self.verticalLayoutWidget)
+        self.userview.setObjectName(u"userview")
+
+        self.generalcontainer.addWidget(self.userview)
+
+        self.componentsexpand = QVBoxLayout()
+        self.componentsexpand.setObjectName(u"componentsexpand")
+        self.expandcomponent = QPushButton(self.verticalLayoutWidget)
+        self.expandcomponent.setObjectName(u"expandcomponent")
+
+        self.componentsexpand.addWidget(self.expandcomponent)
+
+        self.select_add = QHBoxLayout()
+        self.select_add.setObjectName(u"select_add")
+        self.selectcomponenttype = QComboBox(self.verticalLayoutWidget)
+        self.selectcomponenttype.setObjectName(u"selectcomponenttype")
+
+        self.select_add.addWidget(self.selectcomponenttype)
+
+        self.addcomponent = QPushButton(self.verticalLayoutWidget)
+        self.addcomponent.setObjectName(u"addcomponent")
+
+        self.select_add.addWidget(self.addcomponent)
+
+
+        self.componentsexpand.addLayout(self.select_add)
+
+        self.dynamiccomponentsection = QVBoxLayout()
+        self.dynamiccomponentsection.setObjectName(u"dynamiccomponentsection")
+
+        self.componentsexpand.addLayout(self.dynamiccomponentsection)
+
+
+        self.generalcontainer.addLayout(self.componentsexpand)
+
+        self.addeventbutton = QPushButton(self.verticalLayoutWidget)
+        self.addeventbutton.setObjectName(u"addeventbutton")
+
+        self.generalcontainer.addWidget(self.addeventbutton)
+
+        self.eventform.setWidget(self.scrollAreaWidgetContents_2)
+        self.temp = QWidget(self.page_addevent)
+        self.temp.setObjectName(u"temp")
+        self.temp.setGeometry(QRect(0, 160, 300, 50))
+        self.horizontalLayoutWidget_2 = QWidget(self.temp)
+        self.horizontalLayoutWidget_2.setObjectName(u"horizontalLayoutWidget_2")
+        self.horizontalLayoutWidget_2.setGeometry(QRect(0, 0, 301, 51))
+        self.horizontalLayout = QHBoxLayout(self.horizontalLayoutWidget_2)
+        self.horizontalLayout.setObjectName(u"horizontalLayout")
+        self.horizontalLayout.setContentsMargins(0, 0, 0, 0)
+        self.checkBox = QCheckBox(self.horizontalLayoutWidget_2)
+        self.checkBox.setObjectName(u"checkBox")
+
+        self.horizontalLayout.addWidget(self.checkBox)
+
+        self.scrollArea = QScrollArea(self.horizontalLayoutWidget_2)
+        self.scrollArea.setObjectName(u"scrollArea")
+        self.scrollArea.setWidgetResizable(True)
+        self.scrollAreaWidgetContents_3 = QWidget()
+        self.scrollAreaWidgetContents_3.setObjectName(u"scrollAreaWidgetContents_3")
+        self.scrollAreaWidgetContents_3.setGeometry(QRect(0, 0, 278, 42))
+        self.label_3 = QLabel(self.scrollAreaWidgetContents_3)
+        self.label_3.setObjectName(u"label_3")
+        self.label_3.setGeometry(QRect(0, 0, 250, 40))
+        font3 = QFont()
+        font3.setFamilies([u"Arial"])
+        font3.setPointSize(30)
+        font3.setBold(True)
+        self.label_3.setFont(font3)
+        self.scrollArea.setWidget(self.scrollAreaWidgetContents_3)
+
+        self.horizontalLayout.addWidget(self.scrollArea)
+
+        self.pages.addWidget(self.page_addevent)
         self.page_set = QWidget()
         self.page_set.setObjectName(u"page_set")
         self.table_user = QTableView(self.page_set)
@@ -178,6 +316,7 @@ class Ui_MainWindow(object):
 
         self.retranslateUi(MainWindow)
 
+        self.userinfo.setCurrentIndex(0)
         self.b_showhidebutton.setDefault(False)
 
 
@@ -192,11 +331,11 @@ class Ui_MainWindow(object):
         self.b_settings.setText(QCoreApplication.translate("MainWindow", u"Settings", None))
         self.l_sidebar.setText(QCoreApplication.translate("MainWindow", u"Sidebar", None))
         self.tb_Username.setText("")
-        self.tb_Username.setPlaceholderText(QCoreApplication.translate("MainWindow", u"Username", None))
+        self.tb_Username.setPlaceholderText(QCoreApplication.translate("MainWindow", u"Enter your username here", None))
         self.l_Login.setText(QCoreApplication.translate("MainWindow", u"Login", None))
         self.tb_Password.setInputMask("")
         self.tb_Password.setText("")
-        self.tb_Password.setPlaceholderText(QCoreApplication.translate("MainWindow", u"Password", None))
+        self.tb_Password.setPlaceholderText(QCoreApplication.translate("MainWindow", u"Enter your password here", None))
         self.b_showhidebutton.setText("")
         self.l_Username.setText(QCoreApplication.translate("MainWindow", u"Username", None))
         self.l_Password.setText(QCoreApplication.translate("MainWindow", u"Password", None))
@@ -206,5 +345,16 @@ class Ui_MainWindow(object):
         self.label_2.setText(QCoreApplication.translate("MainWindow", u"Password", None))
         self.Profile.setText(QCoreApplication.translate("MainWindow", u"Profile", None))
         self.Select.setText(QCoreApplication.translate("MainWindow", u"Select", None))
+        self.label_user.setText(QCoreApplication.translate("MainWindow", u"User", None))
+        self.label_title.setText(QCoreApplication.translate("MainWindow", u"Title", None))
+        self.lineEdit.setText(QCoreApplication.translate("MainWindow", u"Title", None))
+        self.label_st.setText(QCoreApplication.translate("MainWindow", u"StartDate", None))
+        self.label_et.setText(QCoreApplication.translate("MainWindow", u"EndDate", None))
+        self.expandusers.setText(QCoreApplication.translate("MainWindow", u"Add Users (Expand)", None))
+        self.expandcomponent.setText(QCoreApplication.translate("MainWindow", u"Add Components", None))
+        self.addcomponent.setText(QCoreApplication.translate("MainWindow", u"add", None))
+        self.addeventbutton.setText(QCoreApplication.translate("MainWindow", u"AddEvent", None))
+        self.checkBox.setText("")
+        self.label_3.setText(QCoreApplication.translate("MainWindow", u"TextLabel", None))
     # retranslateUi
 
