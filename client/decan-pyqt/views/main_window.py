d919bccd2cc7d17f74441efd88298db180d6609e
sebastianji <ec20425@qmul.ac.uk>
2025-02-08 14:45:13 +0000

    insertQuery v3

--- client/decan-pyqt/views/main_window.py
+++ client/decan-pyqt/views/main_window.py
@@ -135,9 +135,7 @@ class MainWindow(QMainWindow):
             'EEnd_Time'         :   (self._ui.AE_DTE_endDTSelect.time()).toPython().strftime('%H:%M:%S'),
             'E_CreatorUserID'   :   userID
         }
-        newEvent['ETitle']
         inputQuery = QSqlQuery(f"INSERT INTO `Events`(`ETitle`, `EStart_Date`, `EEnd_Date`, `EStart_Time`, `EEnd_Time`, `E_CreatorUserID`) VALUES ('{(newEvent['ETitle'])}','{(newEvent['EStart_Date'])}','{(newEvent['EEnd_Date'])}','{(newEvent['EStart_Time'])}','{(newEvent['EEnd_Time'])}','{(newEvent['E_CreatorUserID'])}')")
-        inputQuery.exec()
     
     def returnQuery(self, query: QSqlQuery):
         query.exec()
