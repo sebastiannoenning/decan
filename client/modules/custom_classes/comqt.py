b65cb5b3c4ba7e23cf88eb3607deada3d436763d
sebastianji <ec20425@qmul.ac.uk>
2025-03-10 14:22:15 +0000

    temp

--- client/modules/custom_classes/comqt.py
+++
@@ -1,6 +0,0 @@
-import sys, math, re
-from PySide6.QtCore import QObject
-""" ### Custom Object Manipulation for QT Module
-    #   -   PyQt Custom Functions that are expected to be reused
-"""
-    
--- client/modules/custom_classes/eventlistwrapper.py
+++
@@ -1,8 +0,0 @@
-from PySide6.QtWidgets import QScrollArea, QWidget
-from PySide6.QtCore import Qt
-
-# sets scrollable area for 
-class EventListContainer(QScrollArea):
-    def __init__(self, parent: QWidget | None = ...) -> None:
-        super().__init__(parent)
-        
