4aa34ba34adee75d9e696f9d03e01d76feeafe74
sebastiannoenning <sebastiannoenning@gmail.com>
2025-04-15 19:47:00 +0100

    pushing more code for analysis

--- client/models/event_model.py
+++ client/models/event_model.py
@@ -414,6 +414,217 @@ CREATE TRIGGER `AUTO_Event_Initial_JSON_Format` BEFORE INSERT ON `Events` FOR EA
 $$
 DELIMITER ;"""
         
+def new_dump():
+    """SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `decan`
+--
+
+DROP TABLE IF EXISTS `Events_Tags`;
+DROP TABLE IF EXISTS `Events_Users`;
+DROP TABLE IF EXISTS `Events`;
+DROP VIEW IF EXISTS `EU_FilteredEvents`;
+DROP VIEW IF EXISTS `EU_Events_edit`;
+DROP VIEW IF EXISTS `EU_Events_access`;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `Events`
+--
+
+CREATE TABLE `decan`.`Events` 
+(`EventID` INT(6) NOT NULL AUTO_INCREMENT ,
+ `ETitle` VARCHAR(64) NOT NULL ,
+ `EStart` DATETIME NOT NULL ,
+ `EEnd` DATETIME NOT NULL CHECK (`EEnd` >= `EStart`) , 
+ `EAttributes` JSON NULL ,
+ `E_CreatorUserID` INT(3) NOT NULL ,
+ `E_LocationID` INT(10) NULL ,
+CONSTRAINT `PK_Event` PRIMARY KEY (`EventID`) ,
+CONSTRAINT `FK_User_Event` 
+FOREIGN KEY (`E_CreatorUserID`) REFERENCES `Users`(`UserID`) 
+ON DELETE CASCADE ON UPDATE NO ACTION ,
+FOREIGN KEY (`E_LocationID`) REFERENCES `Address`(`AddressID`) 
+ON DELETE SET NULL ON UPDATE NO ACTION )
+ENGINE = InnoDB;
+
+CREATE TABLE `decan`.`Events_Users` 
+(`EU_EventID` INT(6) NOT NULL ,
+ `EU_UserID` INT(3) NOT NULL ,
+CONSTRAINT `PK_EU` PRIMARY KEY (`EU_EventID`,`EU_UserID`) ,
+CONSTRAINT `FK_EU_Event` 
+FOREIGN KEY (`EU_EventID`) REFERENCES `Events`(`EventID`) 
+ON DELETE CASCADE ON UPDATE NO ACTION ,
+CONSTRAINT `FK_EU_User` 
+FOREIGN KEY (`EU_UserID`) REFERENCES `Users`(`UserID`) 
+ON DELETE CASCADE ON UPDATE NO ACTION )
+ENGINE = InnoDB;
+
+CREATE TABLE `decan`.`Events_Tags` 
+(`ET_EventID` INT(6) NOT NULL ,
+ `ET_TagID` INT(3) NOT NULL ,
+CONSTRAINT `PK_ET` PRIMARY KEY (`ET_EventID`,`ET_TagID`) ,
+CONSTRAINT `FK_ET_Event` 
+FOREIGN KEY (`ET_EventID`) REFERENCES `Events`(`EventID`) 
+ON DELETE CASCADE ON UPDATE NO ACTION ,
+CONSTRAINT `FK_ET_Tags` 
+FOREIGN KEY (`ET_TagID`) REFERENCES `Tags`(`TagID`) 
+ON DELETE CASCADE ON UPDATE NO ACTION )
+ENGINE = InnoDB;
+
+--
+-- Triggers `Events`
+--
+DELIMITER $$
+CREATE TRIGGER `AUTO_Event_Initial_JSON_Format` BEFORE INSERT ON `Events` FOR EACH ROW BEGIN
+	DECLARE object_index INT DEFAULT 0;
+	DECLARE event_type INT DEFAULT 0;
+	DECLARE info JSON DEFAULT JSON_OBJECT('object_index', 0, 'event_type', 0);
+	DECLARE objects JSON DEFAULT JSON_OBJECT();
+	-- If none specified, generate a default value --
+		IF (NEW.EAttributes IS Null OR NEW.Eattributes ='') THEN
+        		SET NEW.EAttributes = JSON_OBJECT("info", info);
+		ELSE
+            -- Extract any $.info attributes & set to local variables -- 
+          		IF JSON_EXTRACT(NEW.EAttributes, "$.info") IS Null THEN
+          			IF (JSON_EXISTS(NEW.EAttributes, "$.object_index") IS NOT Null) THEN
+					SET object_index = CAST(JSON_VALUE(NEW.EAttributes, "$.object_index") AS UNSIGNED);
+					SET info = JSON_SET(info, "object_index", object_index);
+					SET NEW.EAttributes = JSON_REMOVE(NEW.EAttributes, "$.object_index");
+				END IF;
+				IF (JSON_EXISTS(NEW.EAttributes, "$.event_type") IS NOT Null) THEN
+					SET event_type = JSON_VALUE(NEW.EAttributes, "$.event_type");
+					SET info = JSON_SET(info, "event_type", event_type);
+					SET NEW.EAttributes = JSON_REMOVE(NEW.EAttributes, "$.event_type");
+				END IF;
+            			-- Otherwise, if info provided --
+            		ELSE
+            			-- Local store --
+            			SET object_index = CAST(JSON_VALUE(NEW.EAttributes, "$info.object_index") AS UNSIGNED);
+            			SET event_type = JSON_VALUE(NEW.EAttributes, "$info.event_type");
+            			
+            			SET info = JSON_EXTRACT(NEW.EAttributes, "$.info");
+            			SET NEW.EAttributes = JSON_REMOVE(NEW.EAttributes, "$.info");
+            		END IF;
+            		
+            		-- After that if-else execution, any info values should be safely contained in $.info with their individual values in object_index & event_type -- 
+            		-- Check then for existence of keys in absence of objects -- 
+            		IF JSON_EXTRACT(NEW.EAttributes, "$.objects") IS Null THEN
+            			-- Post processing existing keys, check if the attributes leftover are still equal to 0
+            			IF JSON_LENGTH(JSON_KEYS(NEW.EAttributes)) > 0 THEN
+            				SET objects = NEW.EAttributes;
+            			END IF;
+            		-- However, if $.objects is there --
+            		ELSE
+            			-- Extract objects as standalone JSON_OBJECT --
+            			SET objects = JSON_EXTRACT(NEW.EAttributes, "$.objects");
+            		END IF;
+            		
+            		-- Quickly update the object_index if there's a mismatch between the predicted lengths --
+            		IF (JSON_LENGTH(JSON_KEYS(objects)) != object_index) THEN
+            			-- Take the count of objects & update the index --
+            			SET object_index = CAST(JSON_LENGTH(JSON_KEYS(objects)) AS UNSIGNED);
+            			SET info = JSON_SET(info, "object_index", object_index);
+            		END IF;
+            		
+            		-- If object_index is still 0, only add info tab --
+            		IF object_index = 0 THEN
+            			SET NEW.EAttributes = JSON_OBJECT(
+            				'info', info
+            			);
+            		ELSE
+            			SET NEW.EAttributes = JSON_OBJECT(
+            				'info', info,
+            				'objects', objects
+            			); 
+            		END IF;
+            	END IF;
+	END
+$$
+DELIMITER ;
+
+--
+-- Dumping data for table `Events`
+--
+
+INSERT INTO `Events` (`EventID`, `ETitle`, `EStart`, `EEnd`, `EAttributes`, `E_CreatorUserID`, `E_LocationID`) VALUES
+(3, 'Date with Danielle', '2024-04-05 20:00:00', '2024-04-05 20:25:00', '{\"object_index\": 0}', 2, NULL),
+(4, 'Halloween Party', '2024-10-23 23:00:00', '2024-10-24 02:00:00', '{\"object_index\": 1, \"EDescription_1\": \"Bring a costume that shares your initial!\"}', 1, NULL),
+(5, 'Shopping', '2024-10-15 20:00:00', '2024-10-15 20:25:00', '{\"object_index\": 0}', 2, NULL),
+(6, 'James Trip', '2024-10-26 23:00:00', '2024-10-27 00:00:00', '{\"object_index\": 1, \"EDescription_1\": \"Going fishing with James!\"}', 3, NULL),
+(7, 'Christmas', '2024-12-25 00:00:00', '2024-12-25 23:59:00', '{\"object_index\": 1, \"EDescription_1\": \"Santa Clause is coming to town!\"}', 1, NULL),
+(8, 'Christmas Shopping', '2024-12-25 20:00:00', '2024-12-25 20:25:00', '{\"EToDo_1\":{\"EBool\":false,\"ETaskDescription\":\"Buy Gerald Shoes\"},\"object_index\":1}', 1, NULL),
+(9, 'Gaming session', '2024-11-04 23:00:00', '2024-11-05 02:00:00', '{\"object_index\": 0}', 2, NULL),
+(10, 'Fun times at beach', '2024-10-12 23:00:00', '2024-10-13 02:00:00', '{\"object_index\": 1, \"EDescription_1\": \"Bring a costume that shares your initial!\"}', 2, NULL),
+(11, 'Shopping at Walmart', '2024-10-24 19:00:00', '2024-10-24 20:00:00', '{\"EDescription_3\":\"Check if there is any baby carriers as well\",\"EToDo_1\":{\"EBool\":false,\"ETaskDescription\":\"Buy eggs\"},\"EToDo_2\":{\"EBool\":false,\"ETaskDescription\":\"Buy milk\"},\"object_index\":3}', 3, NULL),
+(17, 'Dentist', '2025-02-09 00:00:00', '2025-02-10 00:00:00', '{\"object_index\": 0}', 3, NULL);
+    
+CREATE ALGORITHM = MERGE VIEW `EU_Events_edit` AS SELECT
+    `Events_Users`.`EU_EventID` AS `EU_EventID`
+FROM
+    `Events_Users`
+WHERE
+    `Events_Users`.`EU_UserID` = 3;
+
+CREATE ALGORITHM = MERGE VIEW `EU_Events_access` AS SELECT
+    `E`.*
+FROM
+    (
+        `Events` `E`
+    JOIN `EU_Events_edit` `EU` ON
+        (`E`.`EventID` = `EU`.`EU_EventID`)
+    );
+
+ALTER VIEW `EU_Events_edit` AS 
+SELECT `EU_EventID` FROM `Events_Users` WHERE `EU_UserID` = 2;
+
+SELECT * FROM `Events` WHERE 
+(
+    (
+        (`EStart_Date` <= '2024-04-30' AND COALESCE(`EEnd_Date`,`EStart_Date`) >= '2023-03-00')
+    ) OR (
+        (`EStart_Date` <= '2025-05-30' AND COALESCE(`EEnd_Date`,`EStart_Date`) >= '2024-11-00')
+    )
+)
+AND `E_CreatorUserID` = 2
+
+--
+-- AUTO_INCREMENT for table `Events`
+--
+ALTER TABLE `Events`
+  MODIFY `EventID` int(6) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=21;
+
+--
+-- Dumping data for table `Events_Users`
+--
+
+INSERT INTO `Events_Users` (`EU_EventID`, `EU_UserID`) VALUES
+(3, 1),
+(3, 3),
+(4, 3),
+(5, 1),
+(6, 1),
+(6, 2),
+(8, 2),
+(10, 3);
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+
+#1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near 'ALTER TABLE `Events`
+  MODIFY `EventID` int(6) NOT NULL AUTO_INCREMENT, AUTO...' at line 14"""
+        
 class CEventUserModel(QSqlTableModel): #Access model for view in database
     def __init__(self, /, parent = ..., db = ...):
         super().__init__(parent, db)
